434bb85d81f2b378f3b85d10b7b3bd8049c509ac
AbdulloMRX <nuriddinovabdullo70@gmail.com>
2024-01-07 19:54:39 +0500

    My First React

---
+++ test_react_js/src/FirstProject/header/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+export  function header() {
+  return (
+    
+    <header>
+            <ul>
+                <li>HOME</li>
+                <li>ABOUT</li>
+                <li>SERVICE</li>
+                <li>PORTFOLIO</li>
+                <li>PAGE</li>
+                <li>CONTACT</li>
+            </ul>
+    </header>
+  ) 
+}
